2e9b6205cbec3de0e6e5be79411b1541719acb4e
Favvie <favourprecious3@gmail.com>
2022-04-27 15:40:49 +0100

    logo update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/logo1.png";
 import { FaBars, FaTimes, FaLinkedin, FaGithub } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
@@ -12,7 +12,9 @@ const Navbar = () => {
 	return (
 		<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
 			<div>
-				<img src={Logo} alt="Logo" style={{ width: "50px" }} />
+				<Link to="home" smooth={true} offset={50} duration={500}>
+					<img src={Logo} alt="Logo" style={{ width: "50px" }} />
+				</Link>
 			</div>
 
 			{/* main menu */}
